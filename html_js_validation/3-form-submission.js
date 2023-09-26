5e903ca9c88ca9dcf992df89e5fe4903a878616b
Samih51 <97594964+Samih51@users.noreply.github.com>
2023-09-26 19:52:29 +0300

    Update 3-form-submission.js

--- html_js_validation/3-form-submission.js
+++ html_js_validation/3-form-submission.js
@@ -1,24 +1,25 @@
-function handleFormSubmit(event){
-
+function handleFormSubmit(event) {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let message = document.createElement("p");
-    let messageContainer = document.getElementById("messageContainer");
+    let form = document.getElementById("submitForm");
+    let previousMessage = document.getElementById("message")
 
-    let previousError = document.getElementById("message");
-    if (previousError) {
-        messageContainer.removeChild(previousError);
-    }
-    if(name =="" || email==""){
+    if (previousMessage != null)
+    previousMessage.textContent = "";
+
+    if (name === "" || email === "") {
         message.textContent = "Please fill in all fields";
         message.id = "message";
-        messageContainer.appendChild(message);
-        event.preventDefault()
-    }
-    else{
+        form.append(message);
+        event.preventDefault();
+    } else {
         message.textContent = "Form submitted successfully!";
         message.id = "message";
-        messageContainer.appendChild(message);
+        form.append(message);
+        setTimeout(function () {
+            form.removeChild(message);
+        }, 5000);
+    
     }
-
 }
